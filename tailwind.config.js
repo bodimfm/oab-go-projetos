38024f3885bd60b793fd435135c67b0b556587e3
bodimfm <152702496+bodimfm@users.noreply.github.com>
2025-04-30 07:05:09 -0300

    Adiciona arquivos de configuração

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'oab-blue': '#003366',
+        'oab-gold': '#D4AF37',
+      },
+    },
+  },
+  plugins: [],
+}
